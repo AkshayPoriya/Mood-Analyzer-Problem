fdfefedef4a26efb1edf7158dea59cf0959448b9
AkshayPoriya <akshayporiya1234@gmail.com>
2020-10-14 00:44:06 +0530

    [Akshay] UC7 Change Mood on the fly with Reflection

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -9,9 +9,18 @@ namespace MoodAnalyzerProblem
         {
             try
             {
-                //Calling AnalyseMood using Reflection
-                string mood = MoodAnalyzerReflection.InvokeAnalyseMood("AnalyseMood", "i am in any mood!");
-                Console.WriteLine($"Mood is {mood}!");
+                ///Change Field With Reflection
+                MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood!"); //This instance can also be created with Reflection
+                // Meta information of MoodAnalyzer
+                var moodAnalyzerType = typeof(MoodAnalyzer);
+                // Meta information of messageField 
+                //GetField Returns an object that represents the field with the specified name, if found; otherwise, null.
+                var messageField = moodAnalyzerType.GetField("message", BindingFlags.NonPublic | BindingFlags.Instance);
+                Console.WriteLine("Initial message was : "+messageField.GetValue(moodAnalyzer));
+                Console.WriteLine($"Mood was {moodAnalyzer.AnalyseMood()}!");
+                messageField.SetValue(moodAnalyzer,"I am in happy mood!");
+                Console.WriteLine("Current message is : " + messageField.GetValue(moodAnalyzer));
+                Console.WriteLine($"Mood is {moodAnalyzer.AnalyseMood()}!");
             }
             catch(MoodAnalyzerException ex)
             {
